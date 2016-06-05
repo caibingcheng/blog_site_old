1d094b5459f4f360b87ba98bb8412921f878a26b
caibingcheng <1193376579@qq.com>
2016-06-05 15:30:17 +0800

    page

--- static/all.js
+++ static/all.js
@@ -26,6 +26,7 @@
 		tips.innerHTML="";
 		var tips=document.getElementsByClassName("tips")[0];
 		tips.innerHTML="<a href=\"#top\" id=\"show\" onclick=\"Show_tip();getContent()\"><img src=\"./images/tips.jpg\" alt=\"介绍\"></a><div id=\"son\"></div>";
+		
 	}
 
 	function getContent()
@@ -315,3 +316,21 @@
 			alert("error");
 		}
 	}
+
+//  function phone() {
+//             var sUserAgent = navigator.userAgent.toLowerCase();
+//             var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+//             var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+//             var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+//             var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+//             var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+//             var bIsAndroid = sUserAgent.match(/android/i) == "android";
+//             var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+//             var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+//             if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+            
+//             } else {
+//             }
+//         }
+
+// window.onload=setInterval("phone()",100);
